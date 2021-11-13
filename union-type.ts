b857aa61219fb62462d95ebb4c5eb05df6989252
arjunkarki <aks.arjun.karki@gmail.com>
2021-11-13 11:14:57 +0630

    union-types

---
+++ union-type.ts
@@ -0,0 +1,12 @@
+
+function combine(num1:string|number,num2:number|string){
+    if(typeof num1=="number" && typeof num2=="number"){
+        return num1+num2
+    }else{
+        return num1.toString()+num2.toString()
+    }
+}
+
+console.log(combine("Arjun"," Karki"))
+console.log(combine(20,30))
+
